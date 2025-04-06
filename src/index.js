4f6c21ecfef4df3e8254fe66c24fc6f305fc4cf4
hansi.presidency <hansi.presidency@gmail.com>
2025-04-06 15:05:17 +0000

    Trying

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import IntellivestFrontEnd from './IntellivestFrontEnd';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <IntellivestFrontEnd />
+  </React.StrictMode>
+);
